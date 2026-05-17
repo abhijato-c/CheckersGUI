ec7c4ec3380ea47aafa49a107a389a199083977d
abhijato-c <abhijato.c@gmail.com>
2026-05-17 12:30:53 +0530

    Complete Move Generator

--- EngineSource/MoveGen.h
+++ EngineSource/MoveGen.h
@@ -128,15 +128,51 @@ inline void BKmoves(const checkers &b, MoveList &Moves){
 }
 
 inline MoveList WhiteMoves(const checkers &b){
-    MoveList moves;
-    WKmoves(b,moves);
-    WPmoves(b,moves);
+    if (b.chain == -1){
+        MoveList moves;
+        WKmoves(b,moves);
+        WPmoves(b,moves);
+
+        MoveList caps;
+        for (Move m : moves){
+            if ((m >> 13) & 2)
+                caps.Add(m);
+        }
+
+        if (caps.size() > 0)
+            return caps;
+        return moves;
+    }
+    else{
+        if (b.wp & (1ULL << b.chain))
+            return WPchain(b, b.chain);
+        else
+            return WKchain(b, b.chain);
+    }
 }
 
 inline MoveList BlackMoves(const checkers &b){
-    MoveList moves;
-    BKmoves(b,moves);
-    BPmoves(b,moves);
+    if (b.chain == -1) {
+        MoveList moves;
+        BKmoves(b,moves);
+        BPmoves(b,moves);
+
+        MoveList caps;
+        for (Move m : moves){
+            if ((m >> 13) & 2)
+                caps.Add(m);
+        }
+
+        if (caps.size() > 0)
+            return caps;
+        return moves;
+    }
+    else{
+        if (b.bp & (1ULL << b.chain))
+            return BPchain(b, b.chain);
+        else
+            return BKchain(b, b.chain);
+    }
 }
 
 inline MoveList LegalMoves(const checkers &b){
@@ -148,12 +184,100 @@ inline MoveList LegalMoves(const checkers &b){
     }
 }
 
-inline MoveList Chains(const checkers &b, const Bitboard bb){
+inline MoveList WPchain(const checkers &b, const int i){
+    Bitboard bb = (1ULL << i);
     MoveList Moves;
-    
-    if(b.turn){
-        
-    }
+
+    if (((bb & clear_h & clear_8) != 0) && ((bb << 7) & b.pieces) == 0) 
+        Moves.Add(ctz(bb << 7) << 6 | i);
+    if (((bb & clear_a & clear_8) != 0) && ((bb << 9) & b.pieces) == 0)
+        Moves.Add(ctz(bb << 9) << 6 | i);
+    if (((bb & clear_h & clear_g & clear_8 & clear_7) != 0) && ((bb << 14) & b.pieces) == 0 && ((bb << 7) & b.bp) != 0)
+        Moves.Add((1 << 13) | ctz(bb << 14) << 6 | i);
+    if (((bb & clear_h & clear_g & clear_8 & clear_7) != 0) && ((bb << 14) & b.pieces) == 0 && ((bb << 7) & b.bk) != 0)
+        Moves.Add((2 << 13) | ctz(bb << 14) << 6 | i);
+    if (((bb & clear_a & clear_b & clear_8 & clear_7) != 0) && ((bb << 18) & b.pieces) == 0 && ((bb << 9) & b.bp) != 0)
+        Moves.Add((1 << 13) | ctz(bb << 18) << 6 | i);
+    if (((bb & clear_a & clear_b & clear_8 & clear_7) != 0) && ((bb << 18) & b.pieces) == 0 && ((bb << 9) & b.bk) != 0)
+        Moves.Add((2 << 13) | ctz(bb << 18) << 6 | i);
+}
+
+inline MoveList WKchain(const checkers &b, const int i){
+    Bitboard bb = (1ULL << i);
+    MoveList Moves;
+
+    if (((bb & clear_h & clear_8) != 0) && ((bb << 7) & b.pieces) == 0) 
+        Moves.Add((1 << 12) | ctz(bb << 7) << 6 | i);
+    if (((bb & clear_a & clear_8) != 0) && ((bb << 9) & b.pieces) == 0)
+        Moves.Add((1 << 12) | ctz(bb << 9) << 6 | i);
+    if (((bb & clear_a & clear_1) != 0) && ((bb >> 7) & b.pieces) == 0) 
+        Moves.Add((1 << 12) | ctz(bb >> 7) << 6 | i);
+    if (((bb & clear_h & clear_1) != 0) && ((bb >> 9) & b.pieces) == 0)
+        Moves.Add((1 << 12) | ctz(bb >> 9) << 6 | i);
+    if (((bb & clear_h & clear_g & clear_8 & clear_7) != 0) && ((bb << 14) & b.pieces) == 0 && ((bb << 7) & b.bp) != 0)
+        Moves.Add((1 << 13) | (1 << 12) | ctz(bb << 14) << 6 | i);
+    if (((bb & clear_h & clear_g & clear_8 & clear_7) != 0) && ((bb << 14) & b.pieces) == 0 && ((bb << 7) & b.bk) != 0)
+        Moves.Add((2 << 13) | (1 << 12) | ctz(bb << 14) << 6 | i);
+    if (((bb & clear_a & clear_b & clear_8 & clear_7) != 0) && ((bb << 18) & b.pieces) == 0 && ((bb << 9) & b.bp) != 0)
+        Moves.Add((1 << 13) | (1 << 12) | ctz(bb << 18) << 6 | i);
+    if (((bb & clear_a & clear_b & clear_8 & clear_7) != 0) && ((bb << 18) & b.pieces) == 0 && ((bb << 9) & b.bk) != 0)
+        Moves.Add((2 << 13) | (1 << 12) | ctz(bb << 18) << 6 | i);
+    if (((bb & clear_a & clear_b & clear_1 & clear_2) != 0) && ((bb >> 14) & b.pieces) == 0 && ((bb >> 7) & b.bp) != 0)
+        Moves.Add((1 << 13) | (1 << 12) | ctz(bb >> 14) << 6 | i);
+    if (((bb & clear_a & clear_b & clear_1 & clear_2) != 0) && ((bb >> 14) & b.pieces) == 0 && ((bb >> 7) & b.bk) != 0)
+        Moves.Add((2 << 13) | (1 << 12) | ctz(bb >> 14) << 6 | i);
+    if (((bb & clear_h & clear_g & clear_1 & clear_2) != 0) && ((bb >> 18) & b.pieces) == 0 && ((bb >> 9) & b.bp) != 0)
+        Moves.Add((1 << 13) | (1 << 12) | ctz(bb >> 18) << 6 | i);
+    if (((bb & clear_h & clear_g & clear_1 & clear_2) != 0) && ((bb >> 18) & b.pieces) == 0 && ((bb >> 9) & b.bk) != 0)
+        Moves.Add((2 << 13) | (1 << 12) | ctz(bb >> 18) << 6 | i);
+}
+
+inline MoveList BPchain(const checkers &b, const int i){
+    Bitboard bb = (1ULL << i);
+    MoveList Moves;
+
+    if (((bb & clear_a & clear_1) != 0) && ((bb >> 7) & b.pieces) == 0) 
+        Moves.Add(ctz(bb >> 7) << 6 | i);
+    if (((bb & clear_h & clear_1) != 0) && ((bb >> 9) & b.pieces) == 0)
+        Moves.Add(ctz(bb >> 9) << 6 | i);
+    if (((bb & clear_a & clear_b & clear_1 & clear_2) != 0) && ((bb >> 14) & b.pieces) == 0 && ((bb >> 7) & b.wp) != 0)
+        Moves.Add((1 << 13) | ctz(bb >> 14) << 6 | i);
+    if (((bb & clear_a & clear_b & clear_1 & clear_2) != 0) && ((bb >> 14) & b.pieces) == 0 && ((bb >> 7) & b.wk) != 0)
+        Moves.Add((2 << 13) | ctz(bb >> 14) << 6 | i);
+    if (((bb & clear_h & clear_g & clear_1 & clear_2) != 0) && ((bb >> 18) & b.pieces) == 0 && ((bb >> 9) & b.wp) != 0)
+        Moves.Add((1 << 13) | ctz(bb >> 18) << 6 | i);
+    if (((bb & clear_h & clear_g & clear_1 & clear_2) != 0) && ((bb >> 18) & b.pieces) == 0 && ((bb >> 9) & b.wk) != 0)
+        Moves.Add((2 << 13) | ctz(bb >> 18) << 6 | i);
+}
+
+inline MoveList BKchain(const checkers &b, const int i){
+    Bitboard bb = (1ULL << i);
+    MoveList Moves;
+
+    if (((bb & clear_h & clear_8) != 0) && ((bb << 7) & b.pieces) == 0) 
+        Moves.Add((1 << 12) | ctz(bb << 7) << 6 | i);
+    if (((bb & clear_a & clear_8) != 0) && ((bb << 9) & b.pieces) == 0)
+        Moves.Add((1 << 12) | ctz(bb << 9) << 6 | i);
+    if (((bb & clear_a & clear_1) != 0) && ((bb >> 7) & b.pieces) == 0) 
+        Moves.Add((1 << 12) | ctz(bb >> 7) << 6 | i);
+    if (((bb & clear_h & clear_1) != 0) && ((bb >> 9) & b.pieces) == 0)
+        Moves.Add((1 << 12) | ctz(bb >> 9) << 6 | i);
+    if (((bb & clear_h & clear_g & clear_8 & clear_7) != 0) && ((bb << 14) & b.pieces) == 0 && ((bb << 7) & b.wp) != 0)
+        Moves.Add((1 << 13) | (1 << 12) | ctz(bb << 14) << 6 | i);
+    if (((bb & clear_h & clear_g & clear_8 & clear_7) != 0) && ((bb << 14) & b.pieces) == 0 && ((bb << 7) & b.wk) != 0)
+        Moves.Add((2 << 13) | (1 << 12) | ctz(bb << 14) << 6 | i);
+    if (((bb & clear_a & clear_b & clear_8 & clear_7) != 0) && ((bb << 18) & b.pieces) == 0 && ((bb << 9) & b.wp) != 0)
+        Moves.Add((1 << 13) | (1 << 12) | ctz(bb << 18) << 6 | i);
+    if (((bb & clear_a & clear_b & clear_8 & clear_7) != 0) && ((bb << 18) & b.pieces) == 0 && ((bb << 9) & b.wk) != 0)
+        Moves.Add((2 << 13) | (1 << 12) | ctz(bb << 18) << 6 | i);
+    if (((bb & clear_a & clear_b & clear_1 & clear_2) != 0) && ((bb >> 14) & b.pieces) == 0 && ((bb >> 7) & b.wp) != 0)
+        Moves.Add((1 << 13) | (1 << 12) | ctz(bb >> 14) << 6 | i);
+    if (((bb & clear_a & clear_b & clear_1 & clear_2) != 0) && ((bb >> 14) & b.pieces) == 0 && ((bb >> 7) & b.wk) != 0)
+        Moves.Add((2 << 13) | (1 << 12) | ctz(bb >> 14) << 6 | i);
+    if (((bb & clear_h & clear_g & clear_1 & clear_2) != 0) && ((bb >> 18) & b.pieces) == 0 && ((bb >> 9) & b.wp) != 0)
+        Moves.Add((1 << 13) | (1 << 12) | ctz(bb >> 18) << 6 | i);  
+    if (((bb & clear_h & clear_g & clear_1 & clear_2) != 0) && ((bb >> 18) & b.pieces) == 0 && ((bb >> 9) & b.wk) != 0)
+        Moves.Add((2 << 13) | (1 << 12) | ctz(bb >> 18) << 6 | i);
 }
 
 #endif
--- EngineSource/MovePiece.h
+++ EngineSource/MovePiece.h
@@ -2,6 +2,7 @@
 #define MOVEPIECE_H
 
 #include "Defs.h"
+#include "MoveGen.h"
 
 inline void MoveWhitePiece(Move& mv, checkers &b){
     Bitboard to = (1ULL << ((mv >> 6) & 63));
@@ -83,111 +84,64 @@ inline void MovePiece(Move& mv, checkers &b){
 
     // Chain
     if ((mv >> 13) & 2){
-        
+        MoveList chains;
+        int to = ((mv >> 6) & 63);
+        if (b.turn){
+            if ((mv >> 12) & 1)
+                chains = WKchain(b, to);
+            else
+                chains = WPchain(b, to);
+        }
+        else{
+            if ((mv >> 12) & 1)
+                chains = BKchain(b, to);
+            else
+                chains = BPchain(b, to);
+        }
+
+        if (chains.size() > 0)
+            b.chain = to;
+        else {
+            b.chain = -1;
+            b.turn = !b.turn;
+        }
     }
 }
 
-inline void UnMoveWhite(const Move mv, chess &b){
+inline void UnMoveWhite(const Move mv, checkers &b){
     Bitboard f = (1ULL << (mv & 63));
     Bitboard to = (1ULL << ((mv>>6) & 63));
 
     // Move back piece
-    switch ((mv >> 15) & 7){
-        case 0: b.wp = (b.wp & ~to) | f; break;
-        case 1: b.wn = (b.wn & ~to) | f; break;
-        case 2: b.wb = (b.wb & ~to) | f; break;
-        case 3: b.wr = (b.wr & ~to) | f; break;
-        case 4: b.wq = (b.wq & ~to) | f; break;
-        case 5:
-            b.wk = (b.wk & ~to) | f;
-
-            // Castle
-            if (ctz(f) == 3){
-                // Kingside
-                if (ctz(to) == 1 || ctz(to) == 0) b.wr = (b.wr & ~(1ULL << 2)) | (1ULL << 0);
-                //Queen
-                else if (ctz(to) == 5 || ctz(to) == 7) b.wr = (b.wr & ~(1ULL << 4)) | (1ULL << 7);
-            }
-            break;
-    }
+    
      
     // Undo promotion
-    switch (((mv >> 12) & 7)){
-        case 1: b.wn &= ~to; break;
-        case 2: b.wb &= ~to; break;
-        case 3: b.wr &= ~to; break;
-        case 4: b.wq &= ~to; break;
-    }
+    
 
     // Undo capture
-    switch (((mv >> 18) & 7)){
-        case 1: b.bp |= to; break;
-        case 2: b.bn |= to; break;
-        case 3: b.bb |= to; break;
-        case 4: b.br |= to; break;
-        case 5: b.bq |= to; break;
-        case 6: b.bk |= to; break;
-        case 7: b.bp |= (to >> 8); break;
-    }
+    
 }
 
-inline void UnMoveBlack(const Move mv, chess &b){
+inline void UnMoveBlack(const Move mv, checkers &b){
     Bitboard f = (1ULL << (mv & 63));
     Bitboard to = (1ULL << ((mv>>6) & 63));
 
     // Move back piece
-    switch ((mv >> 15) & 7){
-        case 0: b.bp = (b.bp & ~to) | f; break;
-        case 1: b.bn = (b.bn & ~to) | f; break;
-        case 2: b.bb = (b.bb & ~to) | f; break;
-        case 3: b.br = (b.br & ~to) | f; break;
-        case 4: b.bq = (b.bq & ~to) | f; break;
-        case 5:
-            b.bk = (b.bk & ~to) | f;
-
-            // Castle
-            if (ctz(f) == 59){
-                // Kingside
-                if (ctz(to) == 57 || ctz(to) == 56) b.br = (b.br & ~(1ULL << 58)) | (1ULL << 56);
-                //Queen
-                else if (ctz(to) == 61 || ctz(to) == 63) b.br = (b.br & ~(1ULL << 60)) | (1ULL << 63);
-            }
-            break;
-    }
+    
      
     // Undo promotion
-    switch (((mv >> 12) & 7)){
-        case 1: b.bn &= ~to; break;
-        case 2: b.bb &= ~to; break;
-        case 3: b.br &= ~to; break;
-        case 4: b.bq &= ~to; break;
-    }
+    
 
     // Undo capture
-    switch (((mv >> 18) & 7)){
-        case 1: b.wp |= to; break;
-        case 2: b.wn |= to; break;
-        case 3: b.wb |= to; break;
-        case 4: b.wr |= to; break;
-        case 5: b.wq |= to; break;
-        case 6: b.wk |= to; break;
-        case 7: b.wp |= (to << 8); break;
-    }
+    
 }
 
-inline void UnmovePiece(const Move mv, chess &b){
-    b.WCastleKing = (mv >> 24) & 1;
-    b.WCastleQueen = (mv >> 23) & 1;
-    b.BCastleKing = (mv >> 22) & 1;
-    b.BCastleQueen = (mv >> 21) & 1;
-    b.ep = (int)((mv >> 25) & 127) - 1;
-
+inline void UnmovePiece(const Move mv, checkers &b){
     if (b.turn) UnMoveBlack(mv, b);
     else UnMoveWhite(mv, b);
 
-    b.turn = !b.turn;
-    b.bpcs = b.bp|b.br|b.bn|b.bb|b.bq|b.bk;
-    b.wpcs = b.wp|b.wr|b.wn|b.wb|b.wq|b.wk;
+    b.bpcs = b.bp|b.bk;
+    b.wpcs = b.wp|b.wk;
     b.pieces = b.bpcs|b.wpcs;
 }
 
